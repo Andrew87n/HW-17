09bf65a58414f1b0b3f85dd163406c74e825f5c8
Andrew87n <andlop@ukr.net>
2025-11-08 23:12:22 +0200

    123

---
+++ js.js
@@ -0,0 +1,15 @@
+const textRef = document.querySelector(".text");
+const btnRef = document.querySelector(".btn");
+btnRef.textContent = textRef.textContent;
+
+const imgRef = document.querySelector(".img");
+imgRef.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS0IQhVr9DDJCq61QX28zCoiqDrvezBh5ylw&s";
+
+const linkRef = document.querySelector(".link");
+linkRef.href = "https://www.youtube.com";
+const img1Ref = document.querySelector(".img1");
+img1Ref.alt = "cat";
+
+const listRef = document.querySelector(".list");
+const firstElementChildRef = listRef.firstElementChild;
+firstElementChildRef.value = "hello";
